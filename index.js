96e1a5e8808c64a318c89f0e41a78f0bc91f418d
aimedtuba <cademcnair@aimedtuba.com>
2022-01-08 15:59:13 +0700

    options

--- index.js
+++ index.js
@@ -9,8 +9,28 @@ database.connect()
 app.listen(2000)
 
 app.get('*',async (req,res)=>{
-    let found=await (await database).db("main").collection("redirects").findOne({from:req.path.replace("/","")})
+    let found=await await database.db("main").collection("redirects").findOne({from:req.path.replace("/","")})
     if(found){
-        res.redirect(found.to)
+        if(found.options.limit){
+            if(found.options.limit.left>1){
+                res.redirect(found.to)
+                await database.db("main").collection("redirects").updateOne({from:req.path.replace("/","")},{$inc:{"options.limit.left":-1}})
+            }else{
+                res.redirect(found.to)
+                await database.db("main").collection("redirects").deleteOne({from:found.from})
+            }
+        }else{
+            res.redirect(found.to)
+        }
+        if(found.options.count){
+            await database.db("main").collection("redirects").updateOne({from:found.from},{$inc:{"options.count":1}})
+        }
+        function redirect(){
+            if(found.options.consent){
+                res.send(
+                    `<button onclick="location.replace('${found.to.replace(/'/g,"\\'")}')">go to ${found.to}</button>`
+                )
+            }else{res.redirect(found.to)}
+        }
     }else{res.send(`<button onclick="window.close()">url doesn't exist, sorry (close)</button>`)}
 })
