b0825a3e49499d444d2c860e3af43ed20fbce1ce
aimedtuba <cademcnair@aimedtuba.com>
2022-01-14 23:51:36 +0700

    updates

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ database.connect().then(()=>app.listen())
 
 app.get('*',async (req,res)=>{
     try{
-        let found=await await database.db("main").collection("redirect").findOne({from:req.path.replace("/","")})
+        let found=await database.db("main").collection("redirect").findOne({from:req.path.replace("/","")})
         if(found){
             if(found.options.limit){
                 if(found.options.limit>1){
@@ -19,13 +19,13 @@ app.get('*',async (req,res)=>{
                     await database.db("main").collection("redirect").updateOne({from:req.path.replace("/","")},{$inc:{"options.limit":-1}})
                 }else{
                     redirect()
-                    await database.db("main").collection("redirect").deleteOne({from:found.from})
+                    await database.db("main").collection("redirect").deleteOne({from:req.path.replace("/","")})
                 }
             }else{
                 redirect()
             }
             if(found.options.count){
-                await database.db("main").collection("redirect").updateOne({from:found.from},{$inc:{"options.count":1}})
+                await database.db("main").collection("redirect").updateOne({from:req.path.replace("/","")},{$inc:{"options.count":1}})
             }
             function redirect(){
                 if(found.options.consent){
